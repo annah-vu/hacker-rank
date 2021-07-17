dc5dc297a9b8fda80e882111473754983066ee96
Anna Vu <anna.h.vu9@gmail.com>
2021-07-17 11:58:53 -0500

    off to work

--- sql_challenges.sql
+++ sql_challenges.sql
@@ -195,3 +195,67 @@ Query the total population of all cities in CITY where District is California.
 '''
 select sum(population) from city
 where district = 'California';
+
+'''
+Query the average population of all cities in CITY where District is California.
+'''
+select avg(population) from city
+where district = 'California';
+
+'''
+Query a count of the number of cities in CITY having a Population larger than 10000.
+'''
+select count(id) from city
+where population > 100000;
+
+'''
+Query the average population for all cities in CITY, rounded down to the nearest integer.
+'''
+select round(avg(population)) from city;
+
+'''
+Query the sum of the populations for all Japanese cities in CITY. The COUNTRYCODE for Japan is JPN.
+'''
+select sum(population) from city
+where countrycode = 'JPN';
+
+'''
+Query the difference between the maximum and minimum populations in CITY.
+'''
+select max(population) - min(population) from city;
+
+'''
+We define an employee\'s total earnings to be their monthly salary x months worked, and the maximum total earnings to be the maximum total earnings for any employee in the Employee table. Write a query to find the maximum total earnings for all employees as well as the total number of employees who have maximum total earnings. Then print these values as  space-separated integers.
+'''
+select (salary * months) as earnings, count(*) from employee
+group by earnings
+order by earnings desc
+limit 1;
+
+'''
+Query the following two values from the STATION table:
+The sum of all values in LAT_N rounded to a scale of 2 decimal places.
+The sum of all values in LONG_W rounded to a scale of 2 decimal places.
+'''
+select round(sum(lat_n),2), round(sum(long_w),2) from station;
+
+'''
+Query the sum of Northern Latitudes (LAT_N) from STATION having values greater than 38.7880 and less than 137.2345. Truncate your answer to 4 decimal places.
+'''
+select round(sum(lat_n),4) from station
+where lat_n > 38.7880
+and lat_n < 137.2345;
+
+'''
+Query the greatest value of the Northern Latitudes (LAT_N) from STATION that is less than 137.2345. Truncate your answer to 4 decimal places.
+'''
+select round(max(lat_n),4) from station
+where lat_n < 137.2345;
+
+'''
+Query the Western Longitude (LONG_W) for the largest Northern Latitude (LAT_N) in STATION that is less than 137.2345 . Round your answer to 4 decimal places.
+'''
+select round(long_w,4) from station
+where lat_n < 137.2345
+order by lat_n desc
+limit 1
