f3d25480c28b808da7a21b396ea845c2fc86233d
Anna Vu <anna.h.vu9@gmail.com>
2021-07-17 11:18:08 -0500

    3 stars

--- sql_challenges.sql
+++ sql_challenges.sql
@@ -123,3 +123,75 @@ Query the list of CITY names from STATION that do not end with vowels. Your resu
 '''
 select distinct city from station 
 where not (city like '%a' or city like '%e' or city like '%i' or city like '%o' or city like '%u');
+
+'''
+Query the list of CITY names from STATION that either do not start with vowels or do not end with vowels. Your result cannot contain duplicates.
+https://dev.mysql.com/doc/refman/5.6/en/regexp.html for reference
+https://dataschool.com/how-to-teach-people-sql/how-regex-works-in-sql/ also for reference
+'''
+select distinct city from station where city regexp '^[^aeiou]|[^aeiou]$';
+
+'''
+Query the list of CITY names from STATION that do not start with vowels AND do not end with vowels. Your result cannot contain duplicates.
+'''
+select distinct city from station where city regexp '^[^aeiou].*[^aeiou]$';
+
+'''
+Query the Name of any student in STUDENTS who scored higher than 75 Marks. Order your output by the last three characters of each name. If two or more students both have names ending in the same last three characters (i.e.: Bobby, Robby, etc.), secondary sort them by ascending ID.
+'''
+select name from students
+where marks > 75
+order by right(name, 3), id;
+
+'''
+Write a query that prints a list of employee names (i.e.: the name attribute) from the Employee table in alphabetical order.
+where employee_id is an employees ID number, name is their name, months is the total number of months theyve been working for the company, and salary is their monthly salary.
+''' 
+select name from employee
+order by name;
+
+'''
+Write a query that prints a list of employee names (i.e.: the name attribute) for employees in Employee having a salary greater than 2000 per month who have been employees for less than 10 months. Sort your result by ascending employee_id.
+'''
+select name from employee
+where salary > 2000
+and months < 10;
+
+
+'''
+Ketty gives Eve a task to generate a report containing three columns: Name, Grade and Mark. Ketty doesn\'t want the NAMES of those students who received a grade lower than 8. The report must be in descending order by grade -- i.e. higher grades are entered first. If there is more than one student with the same grade (8-10) assigned to them, order those particular students by their name alphabetically. Finally, if the grade is lower than 8, use "NULL" as their name and list them by their grades in descending order. If there is more than one student with the same grade (1-7) assigned to them, order those particular students by their marks in ascending order.
+Write a query to help Eve.
+
+grade = grade level
+mark = score, if lower than 8 then null
+order desc by grade level, then by name
+'''
+
+select if (grades.grade <8, NULL, students.name), grades.grade, students.marks from students 
+join grades on students.marks between grades.min_mark and grades.max_mark
+order by grades.grade DESC, students.Name, students.marks;
+
+/* using alias */
+select if (gr.grade <8, NULL, s.name), gr.grade, s.marks from students as s 
+join grades as gr on s.marks between gr.min_mark and gr.max_mark
+order by gr.grade desc, s.name, s.marks;
+
+'''
+Julia just finished conducting a coding contest, and she needs your help assembling the leaderboard! Write a query to print the respective hacker_id and name of hackers who achieved full scores for more than one challenge. Order your output in descending order by the total number of challenges in which the hacker earned a full score. If more than one hacker received full scores in same number of challenges, then sort them by ascending hacker_id.
+'''
+
+/* select hacker id, and name for people who competed in more than one challenge with submission score = difficulty table score
+order by number of challenges taken, then by ascending hacker_id*/
+select h.hacker_id, h.name from submissions as s
+join hackers as h on s.hacker_id = h.hacker_id
+join challenges as c on s.challenge_id = c.challenge_id
+join difficulty as d on c.difficulty_level = d.difficulty_level
+where s.score = d.score group by h.hacker_id, h.name having count(*)>1
+order by count(*) desc, h.hacker_id;
+
+
+'''
+Query the total population of all cities in CITY where District is California.
+'''
+select sum(population) from city
+where district = 'California';
